24c081b93ea56e88a3ab0bb991cf6c3da9f24512
hazhiiir <hazhiiir@gmail.com>
2025-06-07 21:42:59 +0330

    add event listenrs for length and exclude characters inputs

--- main.js
+++ main.js
@@ -5,7 +5,20 @@ let selectedCharsets = {
   includeSymbols: false,
 };
 
+let length = 4;
+let excludeCharacters = "";
+
 function handleSelectedCharsetsChange(event) {
   const { target } = event;
   selectedCharsets[target.name] = target.checked;
+}
+
+function handleLengthChange(event) {
+  const { target } = event;
+  length = parseInt(target.value);
+}
+
+function handleExcludeCharacters(event) {
+  const { target } = event;
+  excludeCharacters = target.value;
 }
